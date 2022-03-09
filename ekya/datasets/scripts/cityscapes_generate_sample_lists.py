827d5cfb358036fd7e66a549f6afc3711bfc5a34
Romil <romil.bhardwaj@gmail.com>
2022-03-08 16:35:00 -0800

    Add train/test/val splits

--- ekya/datasets/scripts/cityscapes_generate_sample_lists.py
+++ ekya/datasets/scripts/cityscapes_generate_sample_lists.py
@@ -17,20 +17,23 @@ def parse_args():
 
 def main():
     args = parse_args()
-    split = "train"
+    splits = ["train", "test", "val"]
     mode = "fine"
-    images_dir = os.path.join(args.root, 'leftImg8bit', split)
+
     save_dir = args.save_dir
     if save_dir is None:
         save_dir = os.path.join(args.root, 'sample_lists/citywise/')
     os.makedirs(save_dir, exist_ok=True)
-    cities = os.listdir(images_dir)
-    for city in cities:
-        print(f"Generating samples for city {city}")
-        sample_list = CityscapesClassification.generate_sample_list(root=args.root, mode=mode,
-                                                                    splits=[split], subset_cities=[city], min_res=50)
-        out_file_path = os.path.join(save_dir, '{}_{}.csv'.format(city, mode))
-        CityscapesClassification.dump_to_csv(sample_list[split], out_file_path)
+
+    for split in splits:
+        images_dir = os.path.join(args.root, 'leftImg8bit', split)
+        cities = os.listdir(images_dir)
+        for city in cities:
+            print(f"Generating samples for city {city}")
+            sample_list = CityscapesClassification.generate_sample_list(root=args.root, mode=mode,
+                                                                        splits=[split], subset_cities=[city], min_res=50)
+            out_file_path = os.path.join(save_dir, '{}_{}.csv'.format(city, mode))
+            CityscapesClassification.dump_to_csv(sample_list[split], out_file_path)
 
 if __name__ == '__main__':
     main()
